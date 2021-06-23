33547a8c0cd87f31fc3f534e8f0019f8cb596c51
Hannah Foley <72939772+hannahfoley-1@users.noreply.github.com>
2021-06-23 14:26:50 +0100

    Update factorial.s

--- Semester 1/ex-4-4-factorial/factorial.s
+++ Semester 1/ex-4-4-factorial/factorial.s
@@ -8,7 +8,17 @@ Main:
   @ Calculate n!, where n is stored in R1
   @ Store the result in R0
 
-  @ *** your solution goes here ***
+  MOV R0, #1 @result=1
+  MOV R2, R1 @temp=n
+ 
+  WhileMul: 
+  CMP R2, #1     @while temp>1
+  BLS EndWhile   @{
+  MUL R0, R0, R2 @  result=result*n
+  SUB R2, R2, #1 @  temp=temp-1
+  B WhileMul     @} branch back to top
+ 
+  EndWhile: @when temp<1
 
   @ End of program ... check your result
 

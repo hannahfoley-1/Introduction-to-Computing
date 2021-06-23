fff0e02eb46cacdc9a107e02b0201d66ab09c2f4
Hannah Foley <72939772+hannahfoley-1@users.noreply.github.com>
2021-06-23 14:26:13 +0100

    Update max.s

--- Semester 1/ex-4-3-max/max.s
+++ Semester 1/ex-4-3-max/max.s
@@ -5,10 +5,18 @@
 
 Main:
 
-  @ Calculate the maximum of two values a and b
+   @ Calculate the maximum of two values a and b
   @ a is in R1, b is in R2
 
-  @ *** your solution goes here ***
+  CMP R1, R2          @comparing the contents of a and b, a>=b
+  BLT ElseMaxB        @when b is the maximum value, we branch to ElseMaxB
+  MOV R0, R1          @otherwise, a is the maximum value so we store the result of a in R0
+  B EndMax            @we then branch to EndMax so that the program skips over the part wherwe b is max
+
+  ElseMaxB:
+  MOV R0, R2          @when b is max, we move the value of b into the result R0
+
+  EndMax:
 
   @ End of program ... check your result
 

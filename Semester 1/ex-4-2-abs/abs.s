d22cbbd5ce006be2c272ec454e11d047cb9ea406
Hannah Foley <72939772+hannahfoley-1@users.noreply.github.com>
2021-06-23 14:25:28 +0100

    Update abs.s

--- Semester 1/ex-4-2-abs/abs.s
+++ Semester 1/ex-4-2-abs/abs.s
@@ -7,7 +7,16 @@ Main:
 
   @ Compute absolute value of value in R1
 
-  @ *** your solution goes here ***
+  @result = value
+  @if value is less than 0, 
+  @result = 0 - value
+  @if value is greater than 0, end code
+
+  MOV R0, R1
+  CMP R0, #0
+  BGE  EndIfPos  @End If Greater Than/Equal To 0
+  RSB R0, R0, #0
+  EndIfPos:
 
   @ End of program ... check your result
 

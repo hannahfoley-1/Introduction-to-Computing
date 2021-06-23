f8837b0078b7f5054c3131c95f67670b0a468746
Hannah Foley <72939772+hannahfoley-1@users.noreply.github.com>
2021-06-23 14:22:08 +0100

    Update arithmetic.s

--- Semester 1/ex-2-2-arithmetic/arithmetic.s
+++ Semester 1/ex-2-2-arithmetic/arithmetic.s
@@ -7,7 +7,12 @@ Main:
   
   @ Write a program to compute 4x^2+3x
 
-  @ *** your solution goes here ***
+ MUL R0, R1, R1 @ result = x * x
+ LDR R2, =4     @ tmp = 4
+ MUL R0, R2, R0 @ result = 4 * x * x
+ LDR R2, =3     @ tmp = 3
+ MUL R2, R1, R2 @ tmp = x * tmp
+ ADD R0, R0, R2 @ result = result + tmp
 
   @ End of program ... check your result
 

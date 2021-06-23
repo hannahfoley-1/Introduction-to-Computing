923b8e06c73e38c31b5077154a4b11919d0eaabd
Hannah Foley <72939772+hannahfoley-1@users.noreply.github.com>
2021-06-23 14:29:14 +0100

    Update basics.s

--- S1 assignments/asmt-1-1-basics/basics.s
+++ S1 assignments/asmt-1-1-basics/basics.s
@@ -8,7 +8,34 @@ Main:
   @ Write an ARM Assembly Language program to evaluate
   @   x^3 - 4x^2 + 3x + 8
 
-  @ *** your solution goes here ***
+  @X IS IN R1
+   
+  @indices
+  @get x^3
+  MUL R2, R1, R1
+  MUL R2, R2, R1
+  
+  @indices, multiplication
+  @get 4x^2
+  MUL R3, R1, R1
+  LDR R4, =4
+  MUL R5, R4, R3
+
+  @multiplication
+  @get 3x
+  LDR R6, =3
+  MUL R7, R6, R1
+
+  @addition
+  @get 3x+8
+  ADD R8, R7, #8
+
+  @subtraction
+  SUB R9, R2, R5
+
+  @addition
+  ADD R0, R9, R8
+
 
   @ End of program ... check your result
 

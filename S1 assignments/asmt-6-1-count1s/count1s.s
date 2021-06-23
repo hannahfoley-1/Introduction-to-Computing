89becef43d51414c5759e5a9c726c1b23d9c0714
Hannah Foley <72939772+hannahfoley-1@users.noreply.github.com>
2021-06-23 14:40:11 +0100

    Update count1s.s

--- S1 assignments/asmt-6-1-count1s/count1s.s
+++ S1 assignments/asmt-6-1-count1s/count1s.s
@@ -6,9 +6,37 @@
 Main:
 
   
-  @
-  @ write your program here
-  @
+  @ binary value is in r1
+  @ result in r0
+  
+  MOV R2, #0          @ highest1Count = 0
+  MOV R3, #0          @ current1Count = 0
+  MOV R4, #0          @ loopCount = 0
+
+While:   
+  CMP R4, #32         @ while (loopCount <= 32)
+  BHS EndWhile        @{
+  MOVS R1, R1, LSL #1 @ get currentDigit 
+  BCC lastDigitWas0   @ if currentDigit != 0 {
+  ADD R3, R3, #1      @ current1Count = current1Count + 1
+  B IncLoopCount      @}
+  @ else // (if currentDigit == 0)
+lastDigitWas0:  
+  CMP R3, R2         @ if current1Count > highest1Count
+  BLO IncLoopCount   @{
+  MOV R2, R3         @ highest1Count = current1Count
+  MOV R3, #0         @ current1Count = 0 }
+IncLoopCount: 
+  ADD R4, R4, #1     @ loopCount += 1
+  B While            @}
+
+EndWhile:
+  CMP R3, R2         @if currentCount > highestCount
+  BLO Store          @{
+  MOV R2, R3         @ highestCount = currentCount
+Store:               @}
+  MOV R0, R2         @store result
+
 
 
   @ End of program ... check your result

315a4f417007d364cada17a37e44f4a5298c65cc
Hannah Foley <72939772+hannahfoley-1@users.noreply.github.com>
2021-06-23 14:33:01 +0100

    Update divide.s

--- S1 assignments/asmt-2-1-divide/divide.s
+++ S1 assignments/asmt-2-1-divide/divide.s
@@ -8,7 +8,32 @@ Main:
   @ Write an ARM Assembly Language Program that will divide a
   @   value, a, in R2 by another value, b, in R3.
   
-  @ *** your solution goes here ***
+  @a/b 
+  @store quotient in R0
+  @Store remainder in R1
+
+  @subtract b from a until b>a
+  @each time b is subtracted add one to the value stored in quoient
+  @value remaining in a is the remainder
+  @BE CAREFUL OF A VALUES,AND IF VALUES ARE 0
+  @IF A IS LESS THAN B
+  
+  MOV R0, #0
+  MOV R1, #0
+ 
+  CMP R2, #0
+  CMP R3, #0
+  BEQ EndWhile
+ 
+  WhileLabel: @while (b<a)
+  CMP R3, R2 
+  BHI EndWhile @{
+  SUB R2, R2, R3 @a=a-b
+  ADD R0, R0, #1 @quoteint=quoteint+1
+  B WhileLabel @}
+  EndWhile:
+  @quotient=R0
+  MOV R1, R2 @a=R1
 
   @ End of program ... check your result
 
